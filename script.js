e65c4e49a138fa0720406b956669a85465e2d772
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-09 16:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.querySelector(".modal");
+    const openModalBtn = document.getElementById("openModal");
+    const closeModalBtn = document.querySelector(".close-modal");
+
+    openModalBtn.addEventListener("click", function () {
+        modal.style.display = "flex";
+    });
+
+    closeModalBtn.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function (e) {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+});
